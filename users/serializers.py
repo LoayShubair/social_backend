991b22d8692450241a0548abfa0537b35f9fd5d4
Loay Shubair <loayshubair26@gmail.com>
2026-07-09 13:43:16 +0300

    Update user serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,4 +1,4 @@
-"""from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 
@@ -12,22 +12,7 @@ class RegisterSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data["username"],
-            email=validated_data["email"],
+            email=validated_data.get("email", ""),
             password=validated_data["password"],
-        )
-        return user"""
-
-from rest_framework import serializers
-from django.contrib.auth.models import User
-
-
-class RegisterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ["username", "password"]
-
-    def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data["username"], password=validated_data["password"]
         )
         return user
