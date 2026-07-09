65a7ae213c8c433bcafd001e6fd3b699c091d21c
Reem <reembirawi@gmail.com>
2026-07-09 14:19:51 +0300

    Complete posts app with CRUD, comments, likes, and permissions

---
+++ posts/urls.py
@@ -0,0 +1,9 @@
+from rest_framework.routers import DefaultRouter
+
+from . import views
+
+router = DefaultRouter()
+router.register(r'posts', views.PostViewSet, basename='post')
+router.register(r'comments', views.CommentViewSet, basename='comment')
+
+urlpatterns = router.urls
