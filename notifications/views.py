c453f47d2b7915b601bd62b55f74f54b1f09ddc8
Loay Shubair <loayshubair26@gmail.com>
2026-07-12 17:38:09 +0300

    Add mark notification as read endpoint and merge migration

--- notifications/views.py
+++ notifications/views.py
@@ -1,5 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import Notification
 from .serializers import NotificationSerializer
@@ -12,3 +14,14 @@ class NotificationViewSet(viewsets.ReadOnlyModelViewSet):
 
     def get_queryset(self):
         return Notification.objects.filter(user=self.request.user)
+
+    @action(detail=True, methods=["patch"])
+    def mark_as_read(self, request, pk=None):
+        notification = self.get_object()
+
+        notification.is_read = True
+        notification.save()
+
+        return Response(
+            {"message": "Notification marked as read"}, status=status.HTTP_200_OK
+        )
