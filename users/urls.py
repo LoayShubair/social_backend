109daad139d293d2d41b1caa045e54f7d59144b7
Reem <reembirawi@gmail.com>
2026-07-13 16:39:45 +0300

    Add follow API and IsFollowOwner permission

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from .views import RegisterView
+from rest_framework.routers import DefaultRouter
 
+from .views import RegisterView, FollowViewSet
+
+router = DefaultRouter()
+router.register(r'follow', FollowViewSet, basename='follow')
 urlpatterns = [
+    *router.urls,
     path("register/", RegisterView.as_view(), name="register"),
 ]
+
+
